6401a9084750426f94edf95ba9470e64ec68a192
Kek <Kek@kek-mini.local>
2020-01-17 13:55:50 +0800

    edit profile validation done

--- app/src/main/java/ths/ScanPay_User/EditProfileActivity.java
+++ app/src/main/java/ths/ScanPay_User/EditProfileActivity.java
@@ -5,8 +5,12 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -38,6 +42,9 @@ public class EditProfileActivity extends AppCompatActivity {
              changemobile,changegender,changenickname,changeremark,
              changedob,changepin;
 
+    TextView password_error_message,email_error_message,mobile_error_message,gender_error_message,pin_error_message;
+
+    Button savebtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,31 +83,10 @@ public class EditProfileActivity extends AppCompatActivity {
         changeid =(EditText)findViewById(R.id.idedit);
         changeid.setEnabled(false);
 
-        changeid.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus)
-                {
-                    changeid.setEnabled(false);
-                    Toast.makeText(getApplicationContext(),"save id",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+       
 
-        changeidbtn = (TextView)findViewById(R.id.changeidbtn);
-        changeidbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //changefullname.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
-                changeid.setEnabled(true);
-                changeid.requestFocus();
-                changeid.setFocusable(true);
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.showSoftInput(changeid, InputMethodManager.SHOW_IMPLICIT);
-
-
-            }
-        });
+        password_error_message = (TextView) findViewById(R.id.password_error_message_textview);
+        password_error_message.setVisibility(View.INVISIBLE);
 
         changepassword = (EditText)findViewById(R.id.passwordedit);
         changepassword.setEnabled(false);
@@ -113,6 +99,55 @@ public class EditProfileActivity extends AppCompatActivity {
                     changepassword.setEnabled(false);
                     Toast.makeText(getApplicationContext(),"save password",Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        changepassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+
+                if(s.length()>=4)
+                {
+                    if(s.toString().matches("[a-zA-Z]+"))
+                    {
+                        password_error_message.setVisibility(View.VISIBLE);
+                        password_error_message.setText("Should Contain Numbers");
+
+                    }
+                    else if (s.toString().matches("[0-9]+"))
+                    {
+                        password_error_message.setVisibility(View.VISIBLE);
+                        password_error_message.setText("Should Contain Letters");
+                    }
+                    else
+                    {
+                        password_error_message.setVisibility(View.INVISIBLE);
+
+                    }
+
+                }
+                else if (s.length()<4)
+                {
+                    password_error_message.setVisibility(View.VISIBLE);
+                    password_error_message.setText("Should More than 4 char");
+
+                }
+                else
+                {
+                    password_error_message.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
@@ -134,34 +169,48 @@ public class EditProfileActivity extends AppCompatActivity {
         changeemail = (EditText)findViewById(R.id.emailedit);
         changeemail.setEnabled(false);
 
-        changeemail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus)
-                {
-                    changeemail.setEnabled(false);
-                    Toast.makeText(getApplicationContext(),"save password",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        changeemailbtn = (TextView)findViewById(R.id.changeemailbtn);
-        changeemailbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //changefullname.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
-                changeemail.setEnabled(true);
-                changeemail.requestFocus();
-                changeemail.setFocusable(true);
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.showSoftInput(changeemail, InputMethodManager.SHOW_IMPLICIT);
 
 
-            }
-        });
+
 
         changemobile = (EditText)findViewById(R.id.handphoneedit);
         changemobile.setEnabled(false);
+
+        mobile_error_message = (TextView)findViewById(R.id.mobile_error_message_textview);
+        mobile_error_message.setVisibility(View.INVISIBLE);
+
+        changemobile.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().startsWith("0"))
+                {
+
+
+                    changemobile.setText("");
+                }
+
+                if(s.length()==9||s.length()==10)
+                {
+                    mobile_error_message.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    mobile_error_message.setVisibility(View.VISIBLE);
+                    mobile_error_message.setText("Please Insert Valid Phone Number");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
 
         changemobile.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -189,6 +238,224 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
+        changegender = (EditText)findViewById(R.id.genderedit);
+        changegender.setEnabled(false);
+
+        gender_error_message = (TextView)findViewById(R.id.gender_error_message_textview);
+        gender_error_message.setVisibility(View.INVISIBLE);
+
+        changegender.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    changegender.setEnabled(false);
+                    Toast.makeText(getApplicationContext(),"save password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        changegender.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+               if(s.toString().toLowerCase().equals("male")||s.toString().toLowerCase().equals("female"))
+               {
+                   gender_error_message.setVisibility(View.INVISIBLE);
+               }
+               else
+               {
+                   gender_error_message.setVisibility(View.VISIBLE);
+                   gender_error_message.setText("Please insert male or female only");
+
+               }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+        changegenderbtn = (TextView)findViewById(R.id.changegenderbtn);
+        changegenderbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //changefullname.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+                changegender.setEnabled(true);
+                changegender.requestFocus();
+                changegender.setFocusable(true);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(changegender, InputMethodManager.SHOW_IMPLICIT);
+
+
+            }
+        });
+
+
+        changenickname = (EditText)findViewById(R.id.nicknameedit);
+        changenickname.setEnabled(false);
+
+        changenickname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    changenickname.setEnabled(false);
+                    Toast.makeText(getApplicationContext(),"save password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        changenicknamebtn = (TextView)findViewById(R.id.changenicknamebtn);
+        changenicknamebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //changefullname.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+                changenickname.setEnabled(true);
+                changenickname.requestFocus();
+                changenickname.setFocusable(true);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(changenickname, InputMethodManager.SHOW_IMPLICIT);
+
+
+            }
+        });
+
+        changeremark = (EditText)findViewById(R.id.remarkedit);
+        changeremark.setEnabled(false);
+
+        changeremark.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    changeremark.setEnabled(false);
+                    Toast.makeText(getApplicationContext(),"save password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        changeremarkbtn = (TextView)findViewById(R.id.changeremarkbtn);
+        changeremarkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //changefullname.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+                changeremark.setEnabled(true);
+                changeremark.requestFocus();
+                changeremark.setFocusable(true);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(changeremark, InputMethodManager.SHOW_IMPLICIT);
+
+
+            }
+        });
+
+        changedob = (EditText)findViewById(R.id.dobedit);
+        changedob.setEnabled(false);
+
+        changedob.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    changedob.setEnabled(false);
+                    Toast.makeText(getApplicationContext(),"save password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        changedobbtn = (TextView)findViewById(R.id.changedobbtn);
+        changedobbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //changefullname.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+                changedob.setEnabled(true);
+                changedob.requestFocus();
+                changedob.setFocusable(true);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(changedob, InputMethodManager.SHOW_IMPLICIT);
+
+
+            }
+        });
+
+        changepin = (EditText)findViewById(R.id.pinedit);
+        changepin.setEnabled(false);
+        changepin.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        pin_error_message = (TextView)findViewById(R.id.pin_error_message_textview);
+        pin_error_message.setVisibility(View.INVISIBLE);
+
+        changepin.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus)
+                {
+                    changepin.setEnabled(false);
+                    Toast.makeText(getApplicationContext(),"save password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        changepin.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length()>=6)
+                {
+                    pin_error_message.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    pin_error_message.setVisibility(View.VISIBLE);
+                    pin_error_message.setText("Please 6 digit pin number");
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        changepinbtn = (TextView)findViewById(R.id.changepinbtn);
+        changepinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //changefullname.getBackground().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
+                changepin.setEnabled(true);
+                changepin.requestFocus();
+                changepin.setFocusable(true);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(changepin, InputMethodManager.SHOW_IMPLICIT);
+
+
+            }
+        });
+
+
+        savebtn = (Button)findViewById(R.id.save);
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validatealldata();
+            }
+        });
+
+
+
 
         profile_img = (ImageView)findViewById(R.id.profile_img);
         Glide.with(this)  //2
@@ -210,6 +477,20 @@ public class EditProfileActivity extends AppCompatActivity {
         });
 
     }
+
+    public void validatealldata()
+    {
+        if(password_error_message.getVisibility()==View.VISIBLE||
+                mobile_error_message.getVisibility()==View.VISIBLE||gender_error_message.getVisibility()==View.VISIBLE||
+                pin_error_message.getVisibility()==View.VISIBLE)
+        {
+            Toast.makeText(this,"cant save still data wrong insert ",Toast.LENGTH_SHORT).show();
+        }
+    }
+    public boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                .matches();
+    } // end of TextWatcher (email)
 
 
 }
